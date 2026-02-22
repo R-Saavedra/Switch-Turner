0c26cb9ebd3975e4e07b268476f83b6d6be275f6
R-Saavedra <raul.lago@udg.edu>
2026-02-22 14:12:39 +0100

    Delete code/single-point_AMBER/take_energies.sh

--- code/single-point_AMBER/take_energies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## This script takes the energy calculated by AMBER single point
-
-number_line=$(grep -n "ENERGY" $1 | cut -f1 -d: | head -n 1)
-energy_line=$((number_line+1))
-
-awk -v "line=$energy_line" 'NR==line' $1 | awk '{print $2}'
-
-exit 0
